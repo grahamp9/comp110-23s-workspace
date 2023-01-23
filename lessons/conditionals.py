2355d12d659b3af85e9f9ffa188786b17d1b2a89
Graham Phillips <grahamp9@email.unc.edu>
2023-01-23 15:49:27 -0500

    new saves

---
+++ lessons/conditionals.py
@@ -0,0 +1,14 @@
+"""Checks light, if green, says go."""
+
+light: str = input("What color is the light?")
+
+if(light == "green"): 
+    print("Go!")
+    print ("Dont look at your phone")
+
+if (light == "red"):
+    print("Stop!")
+else: print("go report this to the authorities. ")
+    
+
+        
