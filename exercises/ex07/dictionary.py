2e54efb7d45c04d5b7be16b569bc84b64e5410b4
Graham Phillips <grahamp9@email.unc.edu>
2023-04-03 22:10:18 -0400

    the

--- exercises/ex07/dictionary.py
+++ exercises/ex07/dictionary.py
@@ -1,36 +1,36 @@
-"""EX07 a dictionary"""
+"""EX07 a dictionary."""
 __author__ = "730408563"
 
-def invert(old_dict:dict[str, str]) -> dict[str, str]:
+
+def invert(old_dict: dict[str, str]) -> dict[str, str]:
     """Inverts the keys and values from a dictionary."""
-    new_dict: dict [str, str] = {}
+    new_dict: dict[str, str] = {}
     for item in old_dict:
         if old_dict[item] in new_dict:
             raise KeyError("duplicate number detected")
         new_dict[old_dict[item]] = item
-    print(new_dict)
     return new_dict
 
 
-def favorite_color(name_color: dict[str,str]) -> str:
-    """Identifies the color that occurs the most frequently."""
+def favorite_color(name_color: dict[str, str]) -> str:
+    """Identifies the color that occurs the most frequently or whichever was first if there is a tie."""
     color_count: dict[str, int] = {}
     for color in name_color:
         if name_color[color] not in color_count:
-            color_count[color] = 1
+            color_count[name_color[color]] = 1
         else:
-            color_count[color] += 1
-
+            color_count[name_color[color]] += 1
     final_count = 0 
-    common_color = None
-    for color in name_color:
-        if color_count[color] > final_count or (color_count[color] == final_count and color in color_count):
+    common_color: str = ""
+    for color in color_count:
+        if color_count[color] > final_count:
             final_count = color_count[color]
-            common_color = color_count[color]
+            common_color = color
     return common_color
 
 
 def count(val: list[str]) -> dict[str, int]:
+    """Counts how many times each string appears."""
     store: dict[str, int] = {}
     for item in val:
         if item in store:
--- exercises/ex07/dictionary_test.py
+++ exercises/ex07/dictionary_test.py
@@ -1,2 +1,60 @@
-"""EX07 a dictionary - test"""
+"""EX07 a dictionary - test."""
 __author__ = "730408563"
+
+from exercises.ex07.dictionary import invert, favorite_color, count
+import pytest
+
+
+def test_empty_invert() -> None:
+    """Tests invert with an empty dictionary."""
+    test_dict: dict[str, str] = {}
+    assert invert(test_dict) == {}
+
+
+def test_error_invert() -> None:
+    """Tests the when invert reaches a KeyError."""
+    with pytest.raises(KeyError):
+        my_dictionary = {'kris': 'jordan', 'michael': 'jordan'}
+        invert(my_dictionary)
+
+
+def test_real_invert() -> None:
+    """Tests a normal dictionary with strings as inputs."""
+    test_dict: dict[str, str] = {"Graham": "Phillips", "John": "Long", "Jesse": "Young"}
+    assert invert(test_dict) == {"Phillips": "Graham", "Long": "John", "Young": "Jesse"} 
+
+
+def test_empty_favorite_color() -> None:
+    """Tests favorite_color with an empty dictionary."""
+    test_dict: dict[str, str] = {}
+    assert favorite_color(test_dict) == ""
+
+
+def test_real_favorite_color() -> None: 
+    """Tests favorite_color with a normal dictionary."""
+    test_dict: dict[str, str] = {"Graham": "yellow", "John": "blue", "Jesse": "blue", "Paul": "green"}
+    assert favorite_color(test_dict) == "blue"
+
+
+def test_real_favorite_color_1() -> None: 
+    """Tests favorite_color with a tie for most favorite."""
+    test_dict: dict[str, str] = {"Graham": "yellow", "John": "blue", "Jesse": "blue", "Paul": "yellow"}
+    assert favorite_color(test_dict) == "yellow"
+
+
+def test_empty_count() -> None:
+    """Tests count with an empty list."""
+    test_list: list[str] = []
+    assert count(test_list) == {}
+
+
+def test_real_count() -> None:
+    """Tests count with a normal list."""
+    test_list: list[str] = ["graham", "john", "jesse", "jesse", "graham"]
+    assert count(test_list) == {"graham": 2, "john": 1, "jesse": 2}
+
+
+def test_real_count_1() -> None:
+    """Tests count with a longer normal list."""
+    test_list: list[str] = ["graham", "john", "jesse", "jesse", "graham", "paul", "karen", "graham", "graham"]
+    assert count(test_list) == {"graham": 4, "john": 1, "jesse": 2, "paul": 1, "karen": 1}
